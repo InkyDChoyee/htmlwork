887ff781c98567cf89928eebc82c44c28517f24f
InkyDChoyee <choyee0421@naver.com>
2023-12-07 11:41:05 +0900

    feat.jquery_toggle

--- header/bars.js
+++ header/bars.js
@@ -1,7 +1,17 @@
-let toggleBtn = document.querySelector(".navbar_toggleBtn");
-let menu = document.querySelector(".navbar_menu");
+// let toggleBtn = document.querySelector(".navbar_toggleBtn");
+// let menu = document.querySelector(".navbar_menu");
 
-toggleBtn.addEventListener('click', function(){
-    // active라는 이름의 클래스 추가/삭제
-    menu.classList.toggle('active');
-})
+// toggleBtn.addEventListener('click', function(){
+//     // active라는 이름의 클래스 추가/삭제
+//     menu.classList.toggle('active');
+// })
+
+// 제이쿼리로 구현
+$(function(){
+    // alert("test");
+    // toggle() - show()/hide()
+    $(".navbar_toggleBtn").click(function(){
+        $(".navbar_menu").toggle("active");
+    })
+
+});
